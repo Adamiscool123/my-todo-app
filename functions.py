adc3548395c7cec04f7dc28b52b38ce8185f3e72
adami <adamidrissi177@gmail.com>
2025-03-30 15:03:47 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+
+
+
+def get_todos(filepath=r'C:\Users\adami\PycharmProjects\PythonProject\Experiments\todos.txt'):
+    """Read a text file and return the list of to-do items"""
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def get_now(todos_arg, filepath=r'C:\Users\adami\PycharmProjects\PythonProject\Experiments\todos.txt'):
+    """Write a to-do items list in the text file."""
+    with open(filepath, "w") as file:
+        stop_local = file.writelines(todos_arg)
+    return stop_local
+
+
